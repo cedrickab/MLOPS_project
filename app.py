f076a48e64e8321eebed1431595887bc38dec355
NesrineZebair <86290938+NesrineZebair@users.noreply.github.com>
2023-02-13 17:52:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
-#coding : utf-8
-
-##First test version
 import dash
 from dash import Dash 
-from dash import Output, Input, html, dcc
+from dash import Output, Input, html, dcc, State
 from dash.exceptions import PreventUpdate
 
 
@@ -17,24 +14,26 @@ app.layout = html.Div(children=[html.H1("MLOPS"),
     dcc.Input(id="Anime_Type",placeholder="Anime Type"),
     dcc.Input(id="Anime_Producer",placeholder="Anime Producer"),
     dcc.Input(id="Anime_Studio",placeholder="Anime Studio"),
+    html.Button('Submit', id='btn'),
     html.Div(id="result")
     
 ])
 
 @app.callback(Output(component_id="result",component_property="children"),
-              [Input(component_id="Anime_Title",component_property="value"),
-               Input(component_id="Anime_Genre(s)",component_property="value"),
-               Input(component_id="Anime_Description",component_property="value"),
-               Input(component_id="Anime_Type",component_property="value"),
-               Input(component_id="Anime_Producer",component_property="value"),
-               Input(component_id="Anime_Studio",component_property="value")
+              [Input(component_id="btn",component_property='n_clicks')],
+              [State(component_id="Anime_Title",component_property="value"),
+               State(component_id="Anime_Genre(s)",component_property="value"),
+               State(component_id="Anime_Description",component_property="value"),
+               State(component_id="Anime_Type",component_property="value"),
+               State(component_id="Anime_Producer",component_property="value"),
+               State(component_id="Anime_Studio",component_property="value")
     ])
-def display_result(Anime_Title,Anime_Genre,Anime_Description,Anime_Type,Anime_Producer,Anime_Studio):
+def display_result(btn,Anime_Title,Anime_Genre,Anime_Description,Anime_Type,Anime_Producer,Anime_Studio):
     l=[Anime_Title,Anime_Genre,Anime_Description,Anime_Type,Anime_Producer,Anime_Studio]
 
     l2= [type(Anime_Title),type(Anime_Genre),type(Anime_Description),type(Anime_Type),type(Anime_Producer),type(Anime_Studio)]
     
-    if all(x!=None for x in l):
+    if all(x!=None for x in l) or btn == None:
         raise PreventUpdate
     else:
 
