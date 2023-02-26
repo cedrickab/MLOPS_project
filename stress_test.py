929332aeb7b0c12bf2acf0fa0981d1e870c7b12d
cedrickab <70320913+cedrickab@users.noreply.github.com>
2023-02-26 13:53:39 +0100

    Update stress_test.py

--- stress_test.py
+++ stress_test.py
@@ -7,6 +7,8 @@ Created on Sat Feb 25 18:14:14 2023
 #locust -f stress_test.py --headless -u 10 -r 10 --run-time 1m --host http://127.0.0.1:5000
 
 from locust import HttpUser, task, between
+import json
+#locust -f stress_test.py --headless -u 10 -r 10 --run-time 1m --host http://127.0.0.1:5000
 
 class PredictUser(HttpUser):
     wait_time = between(5, 10)
@@ -22,4 +24,7 @@ class PredictUser(HttpUser):
             "Anime_Producer": ["Production I.G, Wit Studio"],
             "Anime_Studio": ["Mappa"],
         }
-        self.client.post('/predict', json=data, headers=headers)
+        response = self.client.post('/predict', data=json.dumps(data), headers=headers)
+        if response.status_code == 200:
+            print("OK")
+
